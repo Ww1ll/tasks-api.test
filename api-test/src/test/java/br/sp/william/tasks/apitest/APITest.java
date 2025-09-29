e84991eda21d2a193a14f9ccceb1ae4419b687de
Ww1ll <williambraz65@gmail.com>
2025-09-29 19:42:15 -0300

    feat: add casos de testes

--- api-test/src/test/java/br/sp/william/tasks/apitest/APITest.java
+++ api-test/src/test/java/br/sp/william/tasks/apitest/APITest.java
@@ -1,18 +1,52 @@
 package br.sp.william.tasks.apitest;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class APITest {
 
+    @BeforeClass
+    public static void setup(){
+        RestAssured.baseURI = "http://localhost:8082/tasks-backend";
+    }
+
     @Test
-    public void test(){
+    public void deveRetornarListagemDeTodasAsTarefas(){
         RestAssured.given()
                 .log().all()
                 .when()
-                    .get("http://localhost:8082/tasks-backend/todo")
+                    .get("/todo")
                 .then()
-                .log().all()
+                    .statusCode(200)
                 ;
+    }
+
+    @Test
+    public void deveAdicionarTarefaComSucesso(){
+        RestAssured.given()
+                .body("{\"task\": \"Teste de insercao com restAssured\", \"dueDate\": \"2025-12-09\"}")
+                .contentType(ContentType.JSON)
+                .when()
+                    .post("/todo")
+                .then()
+                    .statusCode(201)
+        ;
+    }
+
+    @Test
+    public void naoDeveInserirTarefaInvalida(){
+        RestAssured.given()
+                    .body("{\"task\": \"Teste de insercao com restAssured\", \"dueDate\": \"2015-12-09\"}")
+                    .contentType(ContentType.JSON)
+                .when()
+                    .post("/todo")
+                .then()
+                    .log().all()
+                    .statusCode(400)
+                    .body("message", CoreMatchers.is("Due date must not be in past"))
+        ;
     }
 }
